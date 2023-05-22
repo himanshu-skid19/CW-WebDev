1acba4c6c75053d5ad34f29285dd986908530980
himanshu-skid19 <h.singhal@iitg.ac.in>
2023-05-22 17:44:43 +0530

    created the code to get the index values of the top3 and rest

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from flask_session import Session
 import requests
+import json
 
 app = Flask(__name__)
 # Configure session to use filesystem (instead of signed cookies)
@@ -11,5 +12,23 @@ Session(app)
 @app.route('/', methods=['GET'])
 def index():
     req = requests.get("https://www.coursehubiitg.in/api/codingweek/contributions")
-    print(req.content)
-    return 'hello'
+    data = req.content
+    json_data = json.loads(data)
+    l = {}
+    j=1
+    for i in json_data:
+        l[i['points']] = j
+        j+=1
+    top3 = []
+    rest = []
+    sorted_points = sorted(l.keys(), reverse=True)
+    k=1
+    for i in sorted_points:
+        if k < 4:
+            top3.append(l[i])
+            k+=1
+        else:
+            rest.append(l[i])
+            k+=1
+            
+    return "hello"
